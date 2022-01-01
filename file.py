a81300d1a8200bfd33c2d11cf09bb15416fb2bb0
Shiva Acharya <cva.acharya8@gmail.com>
2022-01-01 15:58:26 -0800

    Update data structure format on comment

--- file.py
+++ file.py
@@ -1,18 +1,18 @@
 """
 Data Structures:
 get_filenames => list of filenames => Ex. ["Yaris..", "roof_crush_impactor"]
-get_material_info => If MAT card has $ property, return dictionary of dictionaries
-                        material_info = {mat_card_name : {mat_property : mat_value}}
-                        Ex. {"PIECEWISE_LINEAR_PLASTICITY": [{"mid": 200001, "ro": 7.89E-9}, {"mid": 20002,...}]}
-                    Otherwise, returns dictionary with property N/A.
-                        Ex. {"RIGID": [{"N/A": 1, "N/A": 7.6E-9, "N/A" 200.00, "N/A": 0.3, "N/A": 0.0]}
+get_material_info => Ignore property of Material and just extract material value for now
+                        material_info = {mat_card_name : [mat_value]}
+                        Ex. {"PIECEWISE_LINEAR_PLASTICITY": [[200001, 7.89E-9], [20002,...]]}
+
 """
 
-#TODO: #1 Look for better data structure for get_material_info method.
-#TOD0: #2 Better way to exit while loop
+# TODO: #1 Look for better data structure for get_material_info method.
+# TOD0: #2 Better way to exit while loop
 
 
 import os
+
 
 class File:
     def __init__(self, master, filename, filepath):  # master -> True for Master File
@@ -52,7 +52,7 @@ class File:
             if data.startswith('*MAT'):
                 # Do this until next card or section is reached
                 mat_card_name = ""
-                while input_data[line+1].startswith('$#'): # or input_data[line+1].startswith(" "):
+                while input_data[line + 1].startswith('$#'):  # or input_data[line+1].startswith(" "):
                     mat_card_name = data.lstrip("*MAT_").rstrip("\n")
                     # Multi line material property
                     if input_data[line + 1].startswith('$#'):
@@ -64,7 +64,7 @@ class File:
                         mat_value = input_data[line + 2].split()
                     # If no comment, put N/A for property
                     else:
-                        mat_property = ["N/A"] * len(input_data[line+1])
+                        mat_property = ["N/A"] * len(input_data[line + 1])
                         mat_value = input_data[line + 1].split()
 
                     if len(mat_property) > 0:
@@ -83,8 +83,4 @@ class File:
 
                 material_info[mat_card_name] = material_info.get(mat_card_name, []) + [mat_property_value]
 
-
         return material_info
-
-
-
